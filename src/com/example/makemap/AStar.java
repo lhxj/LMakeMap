31e3dffa83f7d94306fb8f930fd415d6be56c173
linghuxj <78433417@qq.com>
2014-06-17 11:39:45 +0800

    Second

--- src/com/example/makemap/AStar.java
+++ src/com/example/makemap/AStar.java
@@ -1,17 +1,20 @@
 package com.example.makemap;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 /**
- * A*算法
- * Created by Administrator on 2014/6/15.
+ * Created by Administrator on 2014/6/16.
  */
 public class AStar {
     private int[][] map;//地图(1可通过 0不可通过)
     private List<Node> openList;//开启列表
     private List<Node> closeList;//关闭列表
+    //4个方向上的权重有一定的规则，重心应该偏左上
+    private final int COST_STRAIGHT_Z = 10;
+    private final int COST_STRAIGHT_Q = 12;
     private int row;//行
     private int column;//列
 
@@ -24,11 +27,11 @@ public class AStar {
     }
 
     //查找坐标（-1：错误，0：没找到，1：找到了）
-    public int search(int x1, int y1, int x2, int y2) {
+    public int search(int x1, int y1, int x2, int y2, int count) {
         if (x1 < 0 || x1 >= row || x2 < 0 || x2 >= row || y1 < 0 || y1 >= column || y2 < 0 || y2 >= column) {
             return -1;
         }
-        if (map[x1][y1] == 0 || map[x2][y2] == 0) {
+        if (map[x1][y1] != 1) {
             return -1;
         }
         Node sNode = new Node(x1, y1, null);
@@ -39,9 +42,9 @@ public class AStar {
             return 0;
         }
         for (Node node : resultList) {
-            map[node.getX()][node.getY()] = -1;
+            map[node.getX()][node.getY()] = count;
         }
-        return 1;
+        return resultList.size();
     }
 
     //查找核心算法
@@ -59,24 +62,25 @@ public class AStar {
             }
             //上
             if ((node.getY() - 1) >= 0) {
-                checkPath(node.getX(), node.getY() - 1, node, eNode);
+                checkPath(node.getX(), node.getY() - 1, node, eNode, COST_STRAIGHT_Z);
             }
             //下
             if ((node.getY() + 1) < column) {
-                checkPath(node.getX(), node.getY() + 1, node, eNode);
+                checkPath(node.getX(), node.getY() + 1, node, eNode, COST_STRAIGHT_Q);
             }
             //左
             if ((node.getX() - 1) >= 0) {
-                checkPath(node.getX() - 1, node.getY(), node, eNode);
+                checkPath(node.getX() - 1, node.getY(), node, eNode, COST_STRAIGHT_Z);
             }
             //右
             if ((node.getX() + 1) < row) {
-                checkPath(node.getX() + 1, node.getY(), node, eNode);
+                checkPath(node.getX() + 1, node.getY(), node, eNode, COST_STRAIGHT_Q);
             }
             //从开启列表中删除
             //添加到关闭列表中
             closeList.add(openList.remove(0));
-            //开启列表中排序，把F值最低的放到最底端            Collections.sort(openList, new NodeFComparator());
+            //开启列表中排序，把F值最低的放到最底端
+            Collections.sort(openList, new NodeFComparator());
         }
         if (isFind) {
             getPath(resultList, node);
@@ -85,7 +89,7 @@ public class AStar {
     }
 
     //查询此路是否能走通
-    private boolean checkPath(int x, int y, Node parentNode, Node eNode) {
+    private boolean checkPath(int x, int y, Node parentNode, Node eNode, int cost) {
         Node node = new Node(x, y, parentNode);
         //查找地图中是否能通过
         if (map[x][y] != 0) {
@@ -100,16 +104,17 @@ public class AStar {
         int index = -1;
         if ((index = isListContains(openList, x, y)) != -1) {
             //G值是否更小，即是否更新G，F值
-            if ((parentNode.getG() + 10) < openList.get(index).getG()) {
+            if ((parentNode.getG() + cost) < openList.get(index).getG()) {
                 node.setParentNode(parentNode);
-                countG(node, eNode, 10);
+                countG(node, eNode, cost);
                 countF(node);
+
                 openList.set(index, node);
             }
         } else {
             //添加到开启列表中
             node.setParentNode(parentNode);
-            count(node, eNode, 10);
+            count(node, eNode, cost);
             openList.add(node);
         }
         return true;
@@ -138,7 +143,7 @@ public class AStar {
     private void count(Node node, Node eNode, int cost) {
         countG(node, eNode, cost);
         countH(node, eNode);
-        countF(eNode);
+        countF(node);
     }
 
     //计算G值
@@ -152,12 +157,43 @@ public class AStar {
 
     //计算H值
     private void countH(Node node, Node eNode) {
-        node.setF(Math.abs(node.getX() - eNode.getX()) + Math.abs(node.getY() - eNode.getY()));
+        node.setF((Math.abs(node.getX() - eNode.getX()) + Math.abs(node.getY() - eNode.getY())) * 10);
     }
 
     //计算F值
     private void countF(Node node) {
-        node.setF(node.getG() + node.getF());
+        node.setF(node.getG() + node.getH());
+    }
+
+    public static void main(String[] args) {
+        int[][] map = new int[][]{// 地图数组
+                {0, 0, 0, 0, 0, 0, 2, 2, 0, 0},
+                {0, 0, 0, 0, 2, 0, 2, 0, 0, 0},
+                {0, 0, 0, 0, 2, 0, 2, 0, 0, 0},
+                {0, 0, 1, 0, 2, 0, 2, 0, 0, 0},
+                {0, 0, 0, 0, 2, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 2, 0, 0, 0, 0, 0}
+        };
+        AStar aStar = new AStar(map, 6, 10);
+        int flag = aStar.search(3, 2, 3, 8, 1);
+        if (flag == -1) {
+            System.out.println("传输数据有误！");
+        } else if (flag == 0) {
+            System.out.println("没找到！");
+        } else {
+            for (int x = 0; x < 6; x++) {
+                for (int y = 0; y < 10; y++) {
+                    if (map[x][y] == 1) {
+                        System.out.print("￥");
+                    } else if (map[x][y] == 0) {
+                        System.out.print("〓");
+                    } else if (map[x][y] == 2) {//输出搜索路径
+                        System.out.print("※");
+                    }
+                }
+                System.out.println();
+            }
+        }
     }
 
 }
@@ -224,6 +260,10 @@ class Node {
     public void setF(int f) {
         this.f = f;
     }
+
+    public String toString() {
+        return "(" + x + "," + y + "," + f + ")";
+    }
 }
 
 //节点比较类
@@ -232,4 +272,5 @@ class NodeFComparator implements Comparator<Node> {
     public int compare(Node o1, Node o2) {
         return o1.getF() - o2.getF();
     }
+
 }
--- src/com/example/makemap/MadeInArray.java
+++ src/com/example/makemap/MadeInArray.java
@@ -1,22 +1,44 @@
 package com.example.makemap;
 
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 
 /**
  * Created by Administrator on 2014/6/15.
  */
 public class MadeInArray {
-    private static final int DEFAULT_WIDTH = 14;
-    private static final int DEFAULT_HEIGHT = 8;
-
+    private static final String TAG = "MadeInArray";
+    //地图默认宽高
+    private static final int DEFAULT_WIDTH = 8;
+    private static final int DEFAULT_HEIGHT = 14;
+    //地图中默认的障碍物为99
+    private static final int DEFAULT_BARRIER = 99;
+    //地图中默认的障碍物数量范围 （如：5至20个随机的，值为15）
+    private static final int DEFAULT_BARRIER_COUNT = 20;
+    //地图参数数组结构
     private int[][] mapArray;
+    //地图路径数量默认设置为80步以上就可以了
+    private static final int DEFAULT_PATH_COUNT = 80;
 
     public MadeInArray() {
         this.mapArray = new int[DEFAULT_WIDTH][DEFAULT_HEIGHT];
+        init();
     }
 
     public MadeInArray(int width, int height) {
         this.mapArray = new int[width][height];
+        init();
+    }
+
+    private void init() {
+        //首先初始化地图参数以后加入障碍物
+        joinBarrierPoint();
+        //然后再根据其绘制地图路径
+        int pathCount = 0;
     }
 
     /**
@@ -24,35 +46,15 @@ public class MadeInArray {
      */
     private void joinBarrierPoint() {
         //随机加入4-8个障碍物（只占一个单元格）
-        int barrierCount = new Random().nextInt(20) + 4;
-        int mapWidth = mapArray.length;
-        int mapHeight = mapArray[0].length;
+        int barrierCount = new Random().nextInt(DEFAULT_BARRIER_COUNT) + 5;
+        int mapWidth = mapWidth();
+        int mapHeight = mapHeigh();
         for (int i = 0; i < barrierCount; i++) {
             int point = new Random().nextInt(mapWidth * mapHeight);
-            int pointWidth = point / mapWidth;
-            int pointHeight = point % mapWidth;
+            int pointHeight = point / mapWidth;
+            int pointWidth = point % mapWidth;
             if (mapArray[pointWidth][pointHeight] == 0) {
-                mapArray[pointWidth][pointHeight] = 2;
-            } else {
-                barrierCount += 1;
-            }
-        }
-    }
-
-    /**
-     * 加入路径的几个转角点的随机坐标
-     */
-    private void joinCornerPoint(int cornerCount) {
-        //随机加入4-8个随机转角点（只占一个单元格）
-        int barrierCount = new Random().nextInt(5) + 4;
-        int mapWidth = mapArray.length;
-        int mapHeight = mapArray[0].length;
-        for (int i = 0; i < barrierCount; i++) {
-            int point = new Random().nextInt(mapWidth * mapHeight);
-            int pointWidth = point / mapWidth;
-            int pointHeight = point % mapWidth;
-            if (mapArray[pointWidth][pointHeight] == 0) {
-                mapArray[pointWidth][pointHeight] = 1;
+                mapArray[pointWidth][pointHeight] = DEFAULT_BARRIER;
             } else {
                 barrierCount += 1;
             }
@@ -62,14 +64,22 @@ public class MadeInArray {
     /**
      * 根据转角点和障碍物计算出路径加入到地图二维数组矩阵中
      */
-    private void getThePath() {
+    private int getThePath(Point x, int count) {
         AStar aStar = new AStar(mapArray, mapWidth(), mapHeigh());
-        for (int i = 0; i < mapWidth(); i++) {
-            for (int j = 0; j < mapHeigh(); j++) {
-
-                aStar.search()
-            }
+        Point y = new Point(new Random().nextInt(5) + x.getX() - 2, new Random().nextInt(mapHeigh()));
+        int flag = aStar.search(x.getX(), x.getY(), y.getX(), y.getY(), count);
+        if (flag == -1 || flag == 0) {
+            System.out.println("-----计算出两点之间的路径出现数据问题或没有计算出两点之间的路径!");
+            flag = getThePath(x, count);
         }
+        return flag;
+    }
+
+    private Point initOrigin() {
+        int x = 0;
+        int y = new Random().nextInt(mapHeigh());
+        mapArray[x][y] = 1;
+        return new Point(x, y);
     }
 
     public int[][] getMapArray() {
@@ -92,6 +102,52 @@ public class MadeInArray {
 
     public static void main(String[] args) {
         MadeInArray map = new MadeInArray();
-        System.out.println(map.getMapArray()[0].length);
+        int[][] array = map.getMapArray();
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                if (array[i][j] == 0) {
+                    System.out.print("〓");
+                    //} else if (array[i][j] == 1) {
+                    //    System.out.print("11");
+                    // } else if (array[i][j] == 2) {
+                    //     System.out.print("※");
+                } else if (array[i][j] == 10) {
+                    System.out.print("00");
+                } else {
+                    System.out.print(array[i][j] + "" + array[i][j]);
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Point {
+    private int x;
+    private int y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String toString() {
+        return "x=" + this.x + "  y=" + this.y;
     }
 }
